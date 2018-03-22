ed2cdf73e36fd11147b667fc73ffb75ce45128cc
mathieuf86 <mathieuferron06@gmail.com>
2018-03-22 09:43:54 +0100

    calcul is working

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
-    
+    $("#input").keypress(function(e) {
+        if(e.which == 13) {
+            var commande = $("#input").val();
+
+            console.log('commande : ', commande); 
+            display(commande);
+
+            if(commande.match(/[=]/gi).length ==1){
+                var result = calcul(commande);
+                display(result);
+            }
+        }
+    });
+
+    function display(result){
+        $("#screen").append('<div> > '+result+' </div>');
+    }
+
+    function calcul(commande){
+        var result;
+        var calcul;
+        for(var i = 1 ; i < commande.length ; i++) {
+            if(calcul != undefined){
+                calcul = calcul + commande[i];
+            }else{
+                calcul = commande[i];
+            }
+        }
+        result = eval(calcul);
+        return result;
+    }
 });
